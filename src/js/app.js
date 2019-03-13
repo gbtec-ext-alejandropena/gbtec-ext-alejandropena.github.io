8b2cada7d350b4bb5745008a55bf3c6cd6ab429f
gbtec-ext-alejandropena <45763092+gbtec-ext-alejandropena@users.noreply.github.com>
2019-03-13 23:10:44 +0100

    Revert "Update particles config"
    This reverts commit 3b05021812d8fdc4d0629d03bda3a667172bfb91.

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   particlesJS('particles-js', {
     "particles": {
       "number": {
-        "value": 90,
+        "value": 60,
         "density": {
           "enable": true,
           "value_area": 800
@@ -34,32 +34,32 @@ document.addEventListener("DOMContentLoaded", function () {
         "value": 0.5,
         "random": false,
         "anim": {
-          "enable": true,
-          "speed": 0.5,
+          "enable": false,
+          "speed": 1,
           "opacity_min": 0.1,
           "sync": false
         }
       },
       "size": {
-        "value": 2,
+        "value": 3,
         "random": true,
         "anim": {
-          "enable": true,
-          "speed": 2,
+          "enable": false,
+          "speed": 19.18081918081918,
           "size_min": 0.1,
           "sync": false
         }
       },
       "line_linked": {
         "enable": true,
-        "distance": 120,
+        "distance": 150,
         "color": "#ffffff",
         "opacity": 0.4,
         "width": 1
       },
       "move": {
         "enable": true,
-        "speed": 2,
+        "speed": 4,
         "direction": "none",
         "random": true,
         "straight": false,
@@ -97,14 +97,14 @@ document.addEventListener("DOMContentLoaded", function () {
           "size": 40,
           "duration": 2,
           "opacity": 8,
-          "speed": 2
+          "speed": 3
         },
         "repulse": {
           "distance": 200,
           "duration": 0.4
         },
         "push": {
-          "particles_nb": 15
+          "particles_nb": 4
         },
         "remove": {
           "particles_nb": 2
